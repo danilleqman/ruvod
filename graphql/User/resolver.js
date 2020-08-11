97ed7a1fe9b3ad37ac444aff958cce21f8a0b83a
Danil Leiman <danilleqman@gmail.com>
2020-08-11 21:37:26 +0300

    create user resolver.js

--- graphql/User/resolver.js
+++ graphql/User/resolver.js
@@ -1,3 +1,22 @@
 const User = require("./model");
-
-exports.resolver = {};
+const Email = require("graphql-type-email");
+exports.resolver = {
+  Query: {
+    user: (_, { id }) => User.findById(id),
+    users: () => User.find(),
+  },
+  Mutation: {
+    createUser: async (_, { input }) => {
+      const user = new User({ ...input });
+      await user.save();
+      return user;
+    },
+    updateUser: async (_, { id, input }) => {
+      return User.findByIdAndUpdate(id, { ...input });
+    },
+    deleteUser: async (_, { id }) => {
+      return User.findByIdAndDelete(id);
+    },
+  },
+  Email: Email,
+};
